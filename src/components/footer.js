8da2b5f4456f8eae942d17a15ad69d2c476d5c4a
Athora190 <jademarte7@gmail.com>
2021-01-13 16:08:38 -0500

    starting footer

--- src/components/footer.js
+++ src/components/footer.js
@@ -1,46 +1,51 @@
+import { blue } from "@material-ui/core/colors";
 import { Block } from "@material-ui/icons";
 
 export default function Footer(){
     let style ={
-        
+        backgroundColor: "#8FC8EB",
         // textAlign: "center",
         // position: "fixed",
-        border: "5px solid black",
+        border: "3px solid black",
         // border_style:"outset",
-        padding:"2%",
-        height:"100%",
-        width:"100%",
-        margins:"1%",
+        padding:"auto",
+        height:"auto",
+        width:"auto",
+        margins:"auto",
         display:"flex",
+        alignItems:"stretch"
       
     };
     let ulStyle ={
         listStyleType: "none",
+        marginBottom: "10px",
+        paddingTop:"20px"
         
     };
-    let separate ={
-        display:"flex",
-        flexAlign:"left"
-    };
     let copyStyle = {
-        display:"flex",
-        flexAlign:"right"
+        paddingLeft:"70%",
+        // display:"flex",
+        // alignSelf:"flex-end",
+        // flexDirection:"row-reverse",
+        // justifyContent: 'space-between',
+        
     };
     return (
 <div style ={style}>
-    <div style = {separate}>
+        <div>
+            <p>Created by:</p>
+            <br/>
+        </div>
+        <div>
+            <ul style={ulStyle}>
         
-        
-        <ul style={ulStyle}>
-        <li>Created by:</li>
             <li><span>Sean Wilson</span></li>
             <li><span>Damian Pascal</span></li>
             <li><span>Jordane Allen</span></li>
             <li><span>Jade Marte</span></li>
             
         </ul>
-        
-    </div>
+        </div>
         <div style={copyStyle}>
             <p>CopyRight.</p>
         </div>
