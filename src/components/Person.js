2a299414f9010089cc8d6a517a8e664b291687a3
JustDame <76237934+JustDame@users.noreply.github.com>
2021-01-13 22:12:29 -0500

    Changes to person

--- src/components/Person.js
+++ src/components/Person.js
@@ -1,10 +1,7 @@
 import { makeStyles } from '@material-ui/core/styles';
 import Card from '@material-ui/core/Card';
-import CardActionArea from '@material-ui/core/CardActionArea';
 import CardContent from '@material-ui/core/CardContent';
-import Button from '@material-ui/core/Button';
 import Typography from '@material-ui/core/Typography';
-import Box from '@material-ui/core/Box';
 import CardMedia from '@material-ui/core/CardMedia';
 const useStyles = makeStyles({
   root: {
@@ -21,6 +18,9 @@ const useStyles = makeStyles({
   pos: {
     marginBottom: 12,
   },
+  image:{ 
+      height:"140px"
+    }
 })
 export default function Person(props) {
     
@@ -28,7 +28,7 @@ export default function Person(props) {
 
     return (
       <Card className={classes.root}>
-       <CardMedia image={props.image}/>
+       <CardMedia image={props.image} className={classes.image}/>
         <CardContent>
           <Typography variant="h5" component="h2">
           {props.name}
