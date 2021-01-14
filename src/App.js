671dc1a19490455c4898cc8212c6b36e8729a492
JustDame <76237934+JustDame@users.noreply.github.com>
2021-01-13 22:23:05 -0500

    Update to Aboutus

--- src/App.js
+++ src/App.js
@@ -19,7 +19,7 @@ function App() {
       <Box mb={10} textAlign="center">
         <Membership/>
       </Box>
-      <Box mb={10}> 
+      <Box mb={10} textAlign="center"> 
       <Aboutus/>
       </Box>
       <Container>
