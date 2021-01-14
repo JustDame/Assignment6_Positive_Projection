302e4852e06eb4ce9431012f22514ba3cd010135
Athora190 <jademarte7@gmail.com>
2021-01-13 20:42:02 -0500

    footer changes

--- src/components/footer.js
+++ src/components/footer.js
@@ -3,51 +3,78 @@ import { Block } from "@material-ui/icons";
 
 export default function Footer(){
     let style ={
-        backgroundColor: "#8FC8EB",
+        backgroundColor: "rgba(143,200,235,0.5)",
         // textAlign: "center",
-        // position: "fixed",
-        border: "3px solid black",
+        position: "relative",
+        border: "3px",
         // border_style:"outset",
-        padding:"auto",
-        height:"auto",
-        width:"auto",
-        margins:"auto",
         display:"flex",
-        alignItems:"stretch"
+        flexDirection:"row",
+        height:"75px",
+        // width:"100%"
+        marginTop:"20px"
+        
+        
       
     };
-    let ulStyle ={
-        listStyleType: "none",
-        marginBottom: "10px",
-        paddingTop:"20px"
+    // let ulStyle ={
+    //     listStyleType: "none",
+    //     marginBottom: "10px",
+    //     margin:"0",
+    //     paddingTop:"20px",
+    //     paddingLeft:"0"
         
-    };
+    // };
+    let pleaseWork ={
+        display:"flex",
+        alignItems:"flex-start",
+        flexDirection:"column",
+        position: "relative",
+        zIndex:100,
+        color:"#E3E3DE"
+
+    }
     let copyStyle = {
-        paddingLeft:"70%",
+        marginLeft:"auto",
+        position: "relative",
+        zIndex:100,
         // display:"flex",
         // alignSelf:"flex-end",
         // flexDirection:"row-reverse",
         // justifyContent: 'space-between',
         
     };
+    let backgroundImage = {
+        position:"absolute",
+        top: 0,
+        left:0,
+        width:"100%",
+        height:"100%",
+        backgroundImage: 'url(/assets/images/hero-bg.jpg)',
+        zIndex:50,
+        opacity:0.4
+    };
     return (
 <div style ={style}>
+    <div style={backgroundImage}></div>
+    <div style={pleaseWork}>
         <div>
             <p>Created by:</p>
-            <br/>
+            {/* <br/> */}
         </div>
         <div>
-            <ul style={ulStyle}>
+           
+        {/* <p> */}
+            <span>Sean Wilson |</span>
+            <span>Damian Pascal |</span>
+            <span>Jordane Allen |</span>
+            <span>Jade Marte</span>
+        {/* </p>  */}
         
-            <li><span>Sean Wilson</span></li>
-            <li><span>Damian Pascal</span></li>
-            <li><span>Jordane Allen</span></li>
-            <li><span>Jade Marte</span></li>
-            
-        </ul>
         </div>
+    </div>
         <div style={copyStyle}>
-            <p>CopyRight.</p>
+            <p>CopyRight 2021.</p>
         </div>
     </div>);
 }
